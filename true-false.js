8120f103d23b8d6a02d7e67ac5e3fa0fb4676501
mrshanto029 <sdks6409@gmail.com>
2022-05-07 10:14:12 +0600

    true-false statements

---
+++ true-false.js
@@ -0,0 +1,15 @@
+// Falsy
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0'," ",[],{},'false'
+const name = "";
+if (name) {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
