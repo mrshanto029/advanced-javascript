2ba7ca77675c477030de8cb48566714b284f034d
mrshanto029 <78254332+mrshanto029@users.noreply.github.com>
2022-05-07 20:50:13 +0600

    double-vs-triple equal

---
+++ double-vs-triple.js
@@ -0,0 +1,15 @@
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
+
+const first = 0;
+const second = false;
+if (first == second) {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
