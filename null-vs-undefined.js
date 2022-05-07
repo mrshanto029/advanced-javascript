f398d455f25f74d4fc99a5d52b2111353ee23184
mrshanto029 <78254332+mrshanto029@users.noreply.github.com>
2022-05-07 11:05:37 +0600

    Add files via upload

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+// Null vs Undefined Expressions...
+// Null = existene d;
+
+let name;
+console.log(name);
+
+function add(num1, num2) {
+    const sum = num1 + num2;
+}
+const result = add(12, 23);
+console.log(result);
+
+function add(num1, num2) {
+    console.log(num2);
+}
+const result = add(15);
+
+const person = { name: "Smart Dude", phone: 01750654256 };
+console.log(person.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 3, 4, 5];
+console.log(ages[12]);
